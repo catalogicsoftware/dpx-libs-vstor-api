eeee77b33bd5231ef22c7751c16d963885512910
Dawid <dmaciak>
2023-12-21 09:38:03 +0100

    feat(DPXN-8865): Implement Create Share Operation

--- src/main/java/com/catalogic/dpx/libs/vstor/service/ShareService.java
+++ src/main/java/com/catalogic/dpx/libs/vstor/service/ShareService.java
@@ -1,7 +1,10 @@
 package com.catalogic.dpx.libs.vstor.service;
 
 import com.catalogic.dpx.libs.vstor.model.Share;
+import com.catalogic.dpx.libs.vstor.model.ShareRequest;
 
 public interface ShareService {
   Share getShareById(int shareId);
+
+  Share createShare(ShareRequest request);
 }
